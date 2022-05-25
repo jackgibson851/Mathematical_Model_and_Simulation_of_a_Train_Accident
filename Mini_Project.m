6c416b74d9ddfa40b8d5f7419adfe3ab8e40aa97
jackgib <jack.gibson@hs-augsburg.de>
2022-05-25 18:29:26 +0100

    First commit

---
+++ Mini_Project.m
@@ -0,0 +1,15 @@
+
+% Model Coefficents 
+m = 80000;        % 80 tonne Mass of each carriage  
+l = 50;           % Length of each carriage is 50m 
+b = 200000;       % the damping coefficant is 200,000 N/(m/s) 
+u_st = 0.15;      % Static Coulomb Friction coefficent 0.15  
+u_kin = 0.05;     % Kinetic Coulomb Friction coefficent 0.05
+v= 13.8889;       % Innitial Velocity of Train 1 in m/s
+normal = 9.81*m;  %Normal Force Mass * Gravity(9.81) used for Friction 
+
+
+
+
+
+
